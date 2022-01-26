d0a9835a589ba84940ffc76ebec5616b6b92b61b
AShulpekov <ash52131@gmail.com>
2022-01-26 18:58:32 +0200

    MILLIX-76: redesign peer detail page

--- src/js/components/peer-info-view.jsx
+++ src/js/components/peer-info-view.jsx
@@ -3,6 +3,7 @@ import {withRouter} from 'react-router-dom';
 import {Button, Col, Row, Table} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import API from '../api/index';
+import moment from 'moment';
 
 
 class PeerInfoView extends Component {
@@ -30,8 +31,26 @@ class PeerInfoView extends Component {
     render() {
         let attributes        = this.state.attributes;
         let simpleAttributes  = [];
+        let jobList  = [];
         let tabularAttributes = [];
         attributes.forEach(ele => {
+            if (ele.attribute_type === 'job_list') {
+                ele.value.forEach(element => {
+                    let jobName = element.job_name.replace(/_/g, ' ');
+                    jobList.push(
+                            <tr>
+                                <td className={'w-20'}>
+                                    {jobName}
+                                </td>
+                                <td className={'text-break'}>
+                                    {element.status}
+                                </td>
+                            </tr>
+                        );
+                    }
+                );
+            }
+
             if (ele.value instanceof Array) {
                 if (ele.attribute_type === 'shard_protocol') {
                     ele.value.forEach(entry => {
@@ -42,19 +61,32 @@ class PeerInfoView extends Component {
             else if (ele.value instanceof Object) {
                 for (let [key, value] of Object.entries(ele.value)) {
                     let attributeType = key.replace(/_/g, ' ');
+                    if (attributeType.includes('date') && (new Date(value)).getTime() > 0){
+                        value = moment.utc(value * 1000).format('YYYY-MM-DD HH:mm:ss')
+                    }
                     simpleAttributes.push(
-                        <Row className="mb-3">
-                            <span>{attributeType}: {value?.toString()}</span>
-                        </Row>
+                        <tr>
+                            <td className={'w-20'}>
+                                {attributeType}
+                            </td>
+                            <td className={'text-break'}>
+                                {value?.toString()}
+                            </td>
+                        </tr>
                     );
                 }
             }
             else {
                 let attributeType = ele.attribute_type.replace(/_/g, ' ');
                 simpleAttributes.push(
-                    <Row className="mb-3">
-                        <span>{attributeType}: {ele.value?.toString()}</span>
-                    </Row>
+                    <tr>
+                        <td className={'w-20'}>
+                            {attributeType}
+                        </td>
+                        <td className={'text-break'}>
+                            {ele.value?.toString()}
+                        </td>
+                    </tr>
                 );
             }
         });
@@ -81,14 +113,41 @@ class PeerInfoView extends Component {
                     </Col>
                 </Row>
                 <div className={'panel panel-filled'}>
-                    <div className={'panel-heading bordered'}>peer attributes</div>
+                    <div className={'panel-heading bordered'}>
+                        node attributes
+                    </div>
                     <div className={'panel-body'}>
-
-                        {simpleAttributes}
+                        <div className={'panel-body'}>
+                            <div className={'section_subtitle'}>
+                                node
+                            </div>
+                            <Table striped bordered hover className={'mb-3'}>
+                                <tbody>
+                                {simpleAttributes}
+                                </tbody>
+                            </Table>
+                        </div>
                     </div>
                 </div>
                 <div className={'panel panel-filled'}>
-                    <div className={'panel-heading bordered'}>shard attributes</div>
+                    <div className={'panel-heading bordered'}>
+                        job list
+                    </div>
+                    <div className={'panel-body'}>
+                        <div className={'panel-body'}>
+                            <div className={'section_subtitle'}>
+                                job
+                            </div>
+                            <Table striped bordered hover className={'mb-3'}>
+                                <tbody>
+                                {jobList}
+                                </tbody>
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+                <div className={'panel panel-filled'}>
+                    <div className={'panel-heading bordered'}>shard list</div>
                     <div className={'panel-body'}>
                         <Table striped bordered hover variant="dark">
                             <thead>
@@ -104,7 +163,7 @@ class PeerInfoView extends Component {
                             {tabularAttributes.map((item, idx) => {
                                 return (
                                     <tr key={idx} className="wallet-node">
-                                        <td>{item.shard_id}</td>
+                                        <td className={'text-break'}>{item.shard_id}</td>
                                         <td>{item.transaction_count}</td>
                                         <td>{item.update_date}</td>
                                         <td>{item.is_required ? 'yes' : 'no'}</td>
