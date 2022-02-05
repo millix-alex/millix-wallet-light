3a3af79aae245126001d0b5bcfd287482c68d81d
Alex Olkhovskii <alex.olkhovskii@gmail.com>
2022-02-05 15:28:45 +0200

    remove unused vendor files

--- src/vendor/luna/luna.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function () {
-    // Handle minimalize left menu
-    $('.left-nav-toggle a').on('click', function(event){
-        event.preventDefault();
-        $("body").toggleClass("nav-toggle");
-    });
-});
