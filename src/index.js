a6298b2603bfdc8167144f6a55e4afcbccc81f45
AShulpekov <ash52131@gmail.com>
2022-04-08 16:06:49 +0300

    MILLIX-102: fix bugs

--- src/index.js
+++ src/index.js
@@ -160,7 +160,8 @@ const getNodeConfig = () => {
                    configType: {}
                };
                configList.forEach(config => {
-                   newConfig.config[config.config_name]     = config.type !== 'string' ? JSON.parse(config.value) : config.value;
+                   console.log(config.config_name, config.value, config.type);
+                   newConfig.config[config.config_name]     = (config.type !== 'string' && config.type !== 'number') ? JSON.parse(config.value) : config.value;
                    newConfig.configType[config.config_name] = config.type;
                });
                store.dispatch(addWalletConfig(newConfig));
