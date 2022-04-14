676668b27bb866fd427d0e035d00a3a9eddd8264
Alex Olkhovskii <alex.olkhovskii@gmail.com>
2022-04-14 14:56:52 +0200

    fix fetch api structure

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -19,29 +19,42 @@ class API {
         }
     }
 
-    fetchApiTangled(url, param = {}) {
-        return this.fetchApi(this.getTangledApiURL() + url, param);
+    fetchApiTangled(url, result_param = {}, method = 'GET') {
+        const absolute_url = this.getTangledApiURL() + url;
+
+        return this.fetchApi(absolute_url, result_param, method);
     }
 
-    fetchApiMillix(url, param = {}) {
-        return this.fetchApi(this.getAuthenticatedMillixApiURL() + url, param);
+    fetchApiMillix(url, result_param = {}, method = 'GET') {
+        const absolute_url = this.getAuthenticatedMillixApiURL() + url;
+
+        return this.fetchApi(absolute_url, result_param, method);
     }
 
-    fetchApi(url, data = { method: 'GET',params:{}, body: {},headers: {} }) {
-
-        if(data.params){
-            let param_string  = '';
-            const param_array = [];
-            Object.keys(data.params).forEach(function(param_key, index) {
-                param_array.push(param_key + '=' + data.params[param_key]);
-            });
-            if (param_array.length > 0) {
-                param_string = '?' + param_array.join('&');
+    fetchApi(url, result_param = {}, method = 'GET') {
+        let data = {};
+        if (method === 'POST') {
+            data = {
+                method : method,
+                headers: {'Content-Type': 'application/json'},
+                body   : JSON.stringify(result_param)
+            };
+        }
+        else {
+            let param_string = '';
+            if (result_param) {
+                const param_array = [];
+                Object.keys(result_param).forEach(function(param_key, index) {
+                    param_array.push(param_key + '=' + result_param[param_key]);
+                });
+                if (param_array.length > 0) {
+                    param_string = '?' + param_array.join('&');
+                }
+                url += param_string;
             }
-            url += param_string;
-        }        
+        }
 
-        return fetch(url,data)
+        return fetch(url, data)
             .then(response => response ? response : Promise.reject())
             .then(response => response.ok ? response.json() : Promise.reject())
             .catch(error => {
@@ -88,71 +101,55 @@ class API {
 
     toggleAdStatus(advertisement_guid) {
         return this.fetchApiTangled(`/C7neErVANMWXWuse`, {
-            params:{
-                p0: encodeURIComponent(JSON.stringify({advertisement_guid: advertisement_guid}))
-            }
+            p0: encodeURIComponent(JSON.stringify({advertisement_guid: advertisement_guid}))
         });
     }
 
     resetAd(advertisementGUID) {
-        return this.fetchApiTangled(`/pKZdzEZrrdPA1jtl`,{
-            params:{
-                p0:advertisementGUID
-            }
+        return this.fetchApiTangled(`/pKZdzEZrrdPA1jtl`, {
+            p0: advertisementGUID
         });
     }
 
     submitAdForm(formData) {
-        return this.fetchApiTangled(`/scWZ0yhuk5hHLd8s`,{
-            params:{
-                p0:encodeURIComponent(JSON.stringify(formData))
-            }
-        });        
+        return this.fetchApiTangled(`/scWZ0yhuk5hHLd8s`, {
+            p0: encodeURIComponent(JSON.stringify(formData))
+        });
     }
 
     requestAdvertisementPayment(advertisementGUID) {
-        return this.fetchApiTangled(`/QYEgbWuFZs5s7Kud`,{
-            params:{
-                p0:advertisementGUID
-            }
-        });       
+        return this.fetchApiTangled(`/QYEgbWuFZs5s7Kud`, {
+            p0: advertisementGUID
+        });
     }
 
     sendTransaction(transactionOutputPayload) {
-        return this.fetchApiMillix(`/XPzc85T3reYmGro1`,{
-            params:{
-                p0:JSON.stringify(transactionOutputPayload)
-            }
+        return this.fetchApiMillix(`/XPzc85T3reYmGro1`, {
+            p0: JSON.stringify(transactionOutputPayload)
         });
     }
 
     getWalletUnspentTransactionOutputList(addressKeyIdentifier, stable) {
-        return this.fetchApiMillix(`/FDLyQ5uo5t7jltiQ`,{
-            params:{
-                p3:addressKeyIdentifier,
-                p4:0,
-                p7:stable,
-                p10:0,
-                p13:'transaction_date desc'
-            }
+        return this.fetchApiMillix(`/FDLyQ5uo5t7jltiQ`, {
+            p3 : addressKeyIdentifier,
+            p4 : 0,
+            p7 : stable,
+            p10: 0,
+            p13: 'transaction_date desc'
         });
     }
 
     getTransactionHistory(addressKeyIdentifier) {
-        return this.fetchApiMillix(`/w9UTTA7NXnEDUXhe`,{
-            params:{
-                p0:addressKeyIdentifier
-            }
-        });        
+        return this.fetchApiMillix(`/w9UTTA7NXnEDUXhe`, {
+            p0: addressKeyIdentifier
+        });
     }
 
     getTransaction(transactionID, shardID) {
-        return this.fetchApiMillix(`/IBHgAmydZbmTUAe8`,{
-            params:{
-                p0:transactionID,
-                p1:shardID
-            }
-        });         
+        return this.fetchApiMillix(`/IBHgAmydZbmTUAe8`, {
+            p0: transactionID,
+            p1: shardID
+        });
     }
 
     getNodeStat() {
@@ -168,41 +165,33 @@ class API {
     }
 
     getFreeOutputs(addressKeyIdentifier) {
-        return this.fetchApiMillix(`/FDLyQ5uo5t7jltiQ`,{
-            params:{
-                p3:addressKeyIdentifier,
-                p4:0,
-                p7:1,
-                p10:0
-            }
+        return this.fetchApiMillix(`/FDLyQ5uo5t7jltiQ`, {
+            p3 : addressKeyIdentifier,
+            p4 : 0,
+            p7 : 1,
+            p10: 0
         });
     }
 
     verifyAddress(address) {
-        return this.fetchApiMillix(`/Xim7SaikcsHICvfQ`,{
-            params:{
-                p0:address
-            }
-        });        
+        return this.fetchApiMillix(`/Xim7SaikcsHICvfQ`, {
+            p0: address
+        });
     }
 
     newSessionWithPhrase(password, mnemonicPhrase) {
         password       = escape_url_param(password);
         mnemonicPhrase = escape_url_param(mnemonicPhrase);
-        return this.fetchApiMillix(`/GktuwZlVP39gty6v`,{
-            params:{
-                p0:password,
-                p1:mnemonicPhrase
-            }
-        });         
+        return this.fetchApiMillix(`/GktuwZlVP39gty6v`, {
+            p0: password,
+            p1: mnemonicPhrase
+        });
     }
 
     newSession(password) {
         password = escape_url_param(password);
-        return this.fetchApiMillix(`/PMW9LXqUv7vXLpbA`,{
-            params:{
-                p0:password
-            }
+        return this.fetchApiMillix(`/PMW9LXqUv7vXLpbA`, {
+            p0: password
         });
     }
 
@@ -223,10 +212,8 @@ class API {
     }
 
     getNodeConfigValueByName(name) {
-        return this.fetchApiMillix(`/2wYLWQfWBa6GLPYs`,{
-            params:{
-                p0:name
-            }
+        return this.fetchApiMillix(`/2wYLWQfWBa6GLPYs`, {
+            p0: name
         });
     }
 
@@ -236,13 +223,9 @@ class API {
                        .then((config) => {
                            return this.fetchApiMillix(`/LLpSTquu4tZL8Nu5`,
                                {
-                                   method : 'POST',
-                                   headers: {'Content-Type': 'application/json'},
-                                   body   : JSON.stringify({
-                                       'p0': config.config_id,
-                                       'p1': value
-                                   })
-                               }
+                                   'p0': config.config_id,
+                                   'p1': value
+                               }, 'POST'
                            );
                        });
         }
@@ -260,39 +243,31 @@ class API {
     }
 
     addWalletAddressVersion(data) {
-        return this.fetchApiMillix(`/hMrav9QMiMyLQosB`,{
-            params:{
-                p0:data.version,
-                p1:data.is_main_network,
-                p2:data.regex_pattern,
-                p3:data.is_default
-            }
-        });       
+        return this.fetchApiMillix(`/hMrav9QMiMyLQosB`, {
+            p0: data.version,
+            p1: data.is_main_network,
+            p2: data.regex_pattern,
+            p3: data.is_default
+        });
     }
 
     removeWalletAddressVersion(data) {
-        return this.fetchApiMillix(`/XgxHmjINTEqANwtS`,{
-            params:{
-                p0:data.version
-            }
-        });  
+        return this.fetchApiMillix(`/XgxHmjINTEqANwtS`, {
+            p0: data.version
+        });
     }
 
     getNodeAboutAttribute() {
-        return this.fetchApiMillix(`/AgsSNTSA0RHmWUkp`,{
-            params:{
-                p0:this.nodeID,
-                p1:'ijDj2VlTyJBl5R4iTCmG'
-            }
-        });        
+        return this.fetchApiMillix(`/AgsSNTSA0RHmWUkp`, {
+            p0: this.nodeID,
+            p1: 'ijDj2VlTyJBl5R4iTCmG'
+        });
     }
 
     listAddresses(addressKeyIdentifier) {
-        return this.fetchApiMillix(`/quIoaHsl8h6IwyEI`,{
-            params:{
-                p0:addressKeyIdentifier,
-            }
-        });        
+        return this.fetchApiMillix(`/quIoaHsl8h6IwyEI`, {
+            p0: addressKeyIdentifier
+        });
     }
 
     getNextAddress() {
@@ -304,19 +279,15 @@ class API {
     }
 
     listActivePeers() {
-        return this.fetchApiMillix(`/0eoUqXNE715mBVqV`,{
-            params:{
-                p0:2,
-                p1:'update_date desc'
-            }
+        return this.fetchApiMillix(`/0eoUqXNE715mBVqV`, {
+            p0: 2,
+            p1: 'update_date desc'
         });
     }
 
     getNodeAttributes(nodeID) {
-        return this.fetchApiMillix(`/AgsSNTSA0RHmWUkp`,{
-            params:{
-                p0:nodeID,
-            }
+        return this.fetchApiMillix(`/AgsSNTSA0RHmWUkp`, {
+            p0: nodeID
         });
     }
 
@@ -339,12 +310,9 @@ class API {
 
         return this.fetchApiMillix('/P2LMh8NsUTkpWAH3',
             {
-                method : 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body   : JSON.stringify({
-                    'p0': payload
-                })
-            }
+                'p0': payload
+            },
+            'POST'
         );
     }
 }
