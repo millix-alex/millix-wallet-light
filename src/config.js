17981b457dfd0f267375b2de935726a7da57b98c
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-04-29 15:57:02 +0100

    revert testnet settings from this branch

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const WALLET_LOG_SIZE_MAX                       = 1000;
-export const GENESIS_TRANSACTION_ID                    = 'BbYAZLcxbx6adN3KwHZSTGjE6VpeDhiJ3ZPrXs6EMAGqDPfi5';
-export const GENESIS_SHARD_ID                          = 'AyAC3kjLtjM4vktAJ5Xq6mbXKjzEqXoSsmGhhgjnkXUvjtF2M';
+export const GENESIS_TRANSACTION_ID                    = '2VngVznbdiQ5tqfWqn2NMP8DijqCbLX79Gygo9yYRVFU6iN35h';
+export const GENESIS_SHARD_ID                          = 'qGuUgMMVmaCvqrvoWG6zARjkrujGMpzJmpNhBgz1y3RjBG7ZR';
 export const CURRENCY_PAIR_SUMMARY_REFRESH_INTERVAL_MS = 10 * 60000;
 const config                                           = {
     WALLET_LOG_SIZE_MAX,
