1c6de9cafaecdcf267f733640b4c99a0374ee42a
AShulpekov <ash52131@gmail.com>
2022-12-19 21:39:02 +0200

    MILLIX-196: fix, time ago ticker

--- src/js/components/utils/reload-time-ticker-view.jsx
+++ src/js/components/utils/reload-time-ticker-view.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import Translation from '../../common/translation';
 import moment from 'moment';
 
 
 function ReloadTimeTickerView(props) {
-    return <>{Translation.getPhrase('06d814962')} {props.last_update_time && moment(props.last_update_time).fromNow()}</>;
+    const [refreshed_ago, set_time_ago] = useState(props.last_update_time && moment(props.last_update_time).fromNow());
+
+    useEffect(() => {
+        set_time_ago(moment(props.last_update_time).fromNow());
+        const interval = setInterval(() => set_time_ago(refreshed_ago && moment(props.last_update_time).fromNow()), 100);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [props.last_update_time]);
+
+    return <>{Translation.getPhrase('06d814962')} {refreshed_ago}</>;
 }
 
 
 export default withRouter(ReloadTimeTickerView);
-
